394a9246e93562e3b3e03750e2818e27e30be341
Nicholas <rbrummer@student.wethinkcode.co.za>
2021-04-29 13:21:30 +0200

    added coding challenge 4

--- coding-challenge-4/script.js
+++ coding-challenge-4/script.js
@@ -1,20 +1,20 @@
 // TEST DATA 1
 
 const bill1 = 275;
-const tip1 = 50 <= bill1 <= 300 ? bill1 * 0.15 : bill1 * 0.2;
+const tip1 = bill1 >= 50 && bill1 <= 300 ? bill1 * 0.15 : bill1 * 0.2;
 
 console.log(`The bill was ${bill1}, the tip was ${tip1}, and the total value was ${bill1 + tip1}`);
 
 // TEST DATA 2
 
 const bill2 = 40;
-const tip2 = 50 <= bill2 <= 300 ? bill2 * 0.15 : bill2 * 0.2;
+const tip2 = bill2 >= 50 && bill2 <= 300 ? bill2 * 0.15 : bill2 * 0.2;
 
 console.log(`The bill was ${bill2}, the tip was ${tip2}, and the total value was ${bill2 + tip2}`);
 
 // TEST DATA 3
 
 const bill3 = 430;
-const tip3 = 50 <= bill3 <= 300 ? bill3 * 0.15 : bill3 * 0.2;
+const tip3 = bill3 >= 50 && bill3 <= 300 ? bill3 * 0.15 : bill3 * 0.2;
 
 console.log(`The bill was ${bill3}, the tip was ${tip3}, and the total value was ${bill3 + tip3}`);
